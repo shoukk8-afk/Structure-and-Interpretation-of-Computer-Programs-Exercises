dab7c6f9debc629b5d9e20f84c859138a31293c3
S.Shohei <shoukk8@gmail.com>
2026-05-18 16:14:54 +0800

    Add geometric functions for points and rectangles

---
+++ Part2/2-3.rkt
@@ -0,0 +1,31 @@
+#lang sicp
+(define (make-point x y) (cons x y))
+(define (x-point p) (car p))
+(define (y-point p) (cdr p))
+
+(define (make-segment a b) (cons a b))
+(define (start-segment line) (car line))
+(define (end-segment line) (cdr line))
+
+(define (midpoint-segment a b)
+  (make-point (/ (+ (x-point a) (x-point b)) 2)
+              (/ (+ (y-point a) (y-point b)) 2)))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define (make-rect a c)
+  (cons a c))
+(define (rect-height r)
+  (- (y-point (cdr r)) (y-point (car r)))) 
+(define (rect-width r)
+  (- (x-point (cdr r)) (x-point (car r))))
+(define (rect-s r)
+  (* (rect-height r) (rect-width r)))
+(define (rect-l r)
+  (* 2 (+ (rect-height r) (rect-width r))))
