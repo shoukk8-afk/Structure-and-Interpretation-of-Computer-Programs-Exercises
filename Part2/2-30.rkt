62dd1f159cb3f31f2b58f32ac49c56aa9faeacae
S.Shohei <shoukk8@gmail.com>
2026-05-29 10:54:05 +0800

    Create 2-30.rkt

---
+++ Part2/2-30.rkt
@@ -0,0 +1,17 @@
+#lang sicp
+(define square (lambda (x) (* x x)))
+
+; 直接的
+(define (square-tree tree)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (square tree))
+        (else (cons (square-tree (car tree))
+                    (square-tree (cdr tree))))))
+
+; mapと再帰を用いる方法
+(define (square-tree tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-tree sub-tree)
+             (square sub-tree)))
+       tree))
